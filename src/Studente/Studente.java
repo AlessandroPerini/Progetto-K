e0b2430657da4002e1937438b29a0c0eea868fb5
cl418646 <cl418646@aga26>
2016-04-05 15:26:17 +0200

    classi: Appunto, Libro, Valutazione, Studente

---
+++ src/Studente/Studente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Studente;
+
+/**
+ *
+ * @author cl418646
+ */
+public class Studente {
+    
+    private String nome;
+    private String cognome;
+    private String email;
+    private String nickname;
+    private int punti;
+    private String telefono; 
+
+    public Studente(String nome, String cognome, String email, String nickname, int punti, String telefono) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.nickname = nickname;
+        this.punti = punti;
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
