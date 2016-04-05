9d0eb849987322ce36184d38a9bc097865dc22fc
te4o <te4o@10.87.2.6>
2016-04-05 15:27:52 +0200

    crazione classi inziali package QeA, Università

---
+++ src/QeA/Risposta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QeA;
+
+/**
+ *
+ * @author te4o
+ */
+public class Risposta {
+    
+    private String titolo;
+    private String domanda;
+    private String id;
+    private int like;
+
+    public Risposta(String titolo, String domanda, String id, int like) {
+        this.titolo = titolo;
+        this.domanda = domanda;
+        this.id = id;
+        this.like = like;
+    }
+ 
+}
