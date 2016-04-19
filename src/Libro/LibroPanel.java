4dac7e25c38b3b28f63e96397f7eafee2f2f4b39
adrian <adrian@adrian-PC.lan>
2016-04-19 19:45:36 +0200

    Sistemata la grafica dei libri

--- src/Libro/LibroPanel.java
+++ src/Libro/LibroPanel.java
@@ -12,6 +12,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
@@ -32,31 +33,40 @@ public class LibroPanel extends JPanel{
         JLabel telefono= new JLabel("Telefono :");
         JLabel email= new JLabel("Email :");
         
-        JTextArea titolo2= new JTextArea(Applicazione.libroAttuale.getTitolo());
-        JTextArea descrizione2= new JTextArea(Applicazione.libroAttuale.getDescrizione());
-        JTextArea prezzo2= new JTextArea(Integer.toString(Applicazione.libroAttuale.getPrezzo()));
-        JTextArea telefono2= new JTextArea(Applicazione.libroAttuale.getTelefono());
-        JTextArea email2= new JTextArea(Applicazione.libroAttuale.getEmail());
+        JTextArea titolo2= new JTextArea(Applicazione.libroAttuale.getTitolo(),2,10);
+        JTextArea descrizione2= new JTextArea(Applicazione.libroAttuale.getDescrizione(),6,10);
+        JLabel prezzo2= new JLabel(Integer.toString(Applicazione.libroAttuale.getPrezzo()));
+        JLabel telefono2= new JLabel(Applicazione.libroAttuale.getTelefono());
+        JLabel email2= new JLabel(Applicazione.libroAttuale.getEmail());
+ 
+        JScrollPane scrollPanel = new JScrollPane(descrizione2);
         
-        titolo2.setPreferredSize(new Dimension(200, 150));
-        descrizione2.setPreferredSize(new Dimension(200, 150));
-        prezzo2.setPreferredSize(new Dimension(200, 150));
-        telefono2.setPreferredSize(new Dimension(200, 150));
-        email2.setPreferredSize(new Dimension(200, 150));
+        descrizione2.setEditable(false);
+        descrizione2.setLineWrap(true);
+        descrizione2.setWrapStyleWord(true);
+        titolo2.setEditable(false);
+        titolo2.setLineWrap(true);
+        
         
         panel.add(titolo);
         panel.add(titolo2);
         panel.add(descrizione);
-        panel.add(descrizione2);
+        panel.add(scrollPanel);
         panel.add(prezzo);
         panel.add(prezzo2);
         panel.add(telefono);
         panel.add(telefono2);
         panel.add(email);
         panel.add(email2);
-  
+        
+        
+        JScrollPane scrollPanel1 = new JScrollPane(panel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPanel1.setPreferredSize(new Dimension(650, 410));
+        scrollPanel1.getVerticalScrollBar().setUnitIncrement(16);
+        
         add(top);
-        add(panel);
+        add(scrollPanel1);
+        
      
 }
 
