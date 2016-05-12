54f99d342ef2d18a388df0438179417563b6f6f6
adrian <adrian@adrian-PC.lan>
2016-05-12 21:13:46 +0200

    modificata la grafica nella lista appunti

--- src/Appunti/Vista/ListaAppuntiPanel.java
+++ src/Appunti/Vista/ListaAppuntiPanel.java
@@ -13,7 +13,10 @@ import Appunti.Ascoltatori.OrdinaListaAppunti;
 import Header.Vista.TopPanel;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
@@ -34,27 +37,35 @@ public class ListaAppuntiPanel extends JPanel{
     private JButton[] appunti = new JButton[Applicazione.listaAppuntiAttuali.size()];
     private JTextField searchField;
     private JLabel noAppunti;
+    private JLabel[] appuntiIco = new JLabel[Applicazione.listaAppuntiAttuali.size()];
     private JComboBox ordina;
+    private JButton addAppunto, searchButton, clearSearch, ordinaButton;
+    private TopPanel top;
+    private JPanel panel, searchPanel, ordinaPanel;
     
     public ListaAppuntiPanel() {
         
-        TopPanel top = new TopPanel("Appunti "+Applicazione.corsoAttuale.getNome());
+        top = new TopPanel("Appunti "+Applicazione.corsoAttuale.getNome());
         
-        JPanel panel = new JPanel(new GridLayout(Applicazione.listaAppuntiAttuali.size()+4, 1));
-        
+       // panel = new JPanel(new GridLayout(Applicazione.listaAppuntiAttuali.size()+4, 1));
+        panel = new JPanel(new GridBagLayout());
+        ordinaPanel = new JPanel(new GridBagLayout());
         //pannello ricerca
-        JPanel searchPanel = new JPanel();
+        searchPanel = new JPanel();
         searchField = new JTextField(30);
         searchField.setHorizontalAlignment(SwingConstants.CENTER);
         searchField.setFont(new Font("Arial", Font.PLAIN, 20));
+        addAppunto = new JButton("Aggiungi Appunto");
+        searchButton = new JButton("Search");
         
-        JButton searchButton = new JButton("Search");
-        
+        GridBagConstraints gbc = new GridBagConstraints();
+        GridBagConstraints gbcd = new GridBagConstraints();
         CercaAppunti cercaAppunti = new CercaAppunti(searchField);
         searchField.addKeyListener(cercaAppunti);
         searchButton.addActionListener(cercaAppunti);
         
-        JButton clearSearch = new JButton("x");
+        clearSearch = new JButton("x");
+        
         clearSearch.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -67,10 +78,10 @@ public class ListaAppuntiPanel extends JPanel{
         searchPanel.add(searchButton);
         // fine pannello ricerca
         
-        panel.add(searchPanel);
+        
         
         //pannello ordina
-        JPanel ordinaPanel = new JPanel();
+        
         
         String[] opzioni = new String[]{"Valutazione", "Nome"};
         ordina = new JComboBox(opzioni);
@@ -78,21 +89,34 @@ public class ListaAppuntiPanel extends JPanel{
             ordina.setSelectedItem(OrdinaListaAppunti.ordineCorrente);
         }
         
-        JButton ordinaButton = new JButton("Ordina");
+        ordinaButton = new JButton("Ordina");
+        
         OrdinaListaAppunti ordinaListaAppunti = new OrdinaListaAppunti(ordina);
         ordinaButton.addActionListener(ordinaListaAppunti);
+        gbc.gridx = 0;
+	gbc.gridy = 0;
+	gbc.insets = new Insets(5, 220, 0, 10);
+	gbc.anchor = GridBagConstraints.LINE_END;
+	ordinaPanel.add(ordina, gbc);
         
-        ordinaPanel.add(ordina);
-        ordinaPanel.add(ordinaButton);
+        gbc.gridx = 1;
+	gbc.gridy = 0;
+	gbc.insets = new Insets(5, 0, 0, 10);
+	gbc.anchor = GridBagConstraints.LINE_START;
+        ordinaPanel.add(ordinaButton,gbc);
+        
+        gbc.gridx = 2;
+	gbc.gridy = 0;
+	gbc.insets = new Insets(5, 100, 0, 10);
+	gbc.anchor = GridBagConstraints.LINE_END;
+        ordinaPanel.add(addAppunto, gbc);
         // fine pannello ordina
         
-        panel.add(ordinaPanel);
         
-        JButton addAppunto = new JButton("Aggiungi Appunto");
         GoToAggiungiAppunto aggiungiAppunto = new GoToAggiungiAppunto();
         addAppunto.addActionListener(aggiungiAppunto);
         
-        panel.add(addAppunto);
+        //panel.add(addAppunto);
         
         GoToAppunto goToAppunto = new GoToAppunto(Applicazione.corsoAttuale.getNome(), Applicazione.facoltàAttuale.getNome());
         
@@ -106,41 +130,62 @@ public class ListaAppuntiPanel extends JPanel{
         }else{
             for (int i = 0; i < Applicazione.listaAppuntiAttuali.size(); i++) {
                 appunti[i] = new JButton();
+                appunti[i].setPreferredSize(new Dimension(200, 30));
                 appunti[i].setText(Applicazione.listaAppuntiAttuali.get(i).getNome());
                 appunti[i].setHorizontalTextPosition(SwingConstants.LEADING);
                 appunti[i].setIconTextGap(50);
                 
+                
+                
                 float media = Applicazione.listaAppuntiAttuali.get(i).getMedia();
                 
                 if((media==0)){
-                    appunti[i].setIcon(new ImageIcon("files\\immagini\\0-star-rating.png"));
+                    appuntiIco[i] = new JLabel(new ImageIcon("files\\immagini\\0-star-rating.png"));
+                    
                 }
                 if((media>0)&&(media<=1)){
-                    appunti[i].setIcon(new ImageIcon("files\\immagini\\1-star-rating.png"));
+                    appuntiIco[i] = new JLabel(new ImageIcon("files\\immagini\\1-star-rating.png"));
+                    
                 }
                 if((media>1)&&(media<=2)){
-                    appunti[i].setIcon(new ImageIcon("files\\immagini\\2-star-rating.png"));
+                    appuntiIco[i] = new JLabel(new ImageIcon("files\\immagini\\2-star-rating.png"));
+                    
                 }
                 if((media>2)&&(media<=3)){
-                    appunti[i].setIcon(new ImageIcon("files\\immagini\\3-star-rating.png"));
+                    appuntiIco[i] = new JLabel(new ImageIcon("files\\immagini\\3-star-rating.png"));
+                    
                 }
                 if((media>3)&&(media<=4)){
-                    appunti[i].setIcon(new ImageIcon("files\\immagini\\4-star-rating.png"));
+                    appuntiIco[i] = new JLabel(new ImageIcon("files\\immagini\\4-star-rating.png"));
+                    
                 }
                 if(media>4){
-                    appunti[i].setIcon(new ImageIcon("files\\immagini\\5-star-rating.png"));
+                    appuntiIco[i] = new JLabel(new ImageIcon("files\\immagini\\5-star-rating.png"));
+                    
                 }
                 
                 appunti[i].addActionListener(goToAppunto);
-                panel.add(appunti[i]);
+                gbcd.gridx = 0;
+                gbcd.gridy = i;
+                gbcd.insets = new Insets(5, 30, 0, 10);
+                gbcd.anchor = GridBagConstraints.LINE_START;
+                panel.add(appunti[i], gbcd);
+                gbcd.gridx = 1;
+                gbcd.gridy = i;
+                gbcd.insets = new Insets(5, 30, 0, 10);
+                gbcd.anchor = GridBagConstraints.LINE_START;
+                panel.add(appuntiIco[i], gbcd);
+                
             }
         }
         
-        JScrollPane scrollPanel = new JScrollPane(panel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        JScrollPane scrollPanel = new JScrollPane(panel,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPanel.setPreferredSize(new Dimension(650, 450));
         scrollPanel.getVerticalScrollBar().setUnitIncrement(16);
         
         add(top);
+        add(searchPanel);
+        add(ordinaPanel);
         add(scrollPanel);
     }
     
